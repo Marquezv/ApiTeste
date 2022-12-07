b6ffefa6a42f13a8f38cd300abe127c4aed73d0a
Vinicius Marques de Oliveira <76068967+Marquezv@users.noreply.github.com>
2022-12-07 16:42:24 -0300

    UserServiceTest
    -whenFindByIdThenReturnAnUserInstance(Test findById)

--- apiTeste/apiTeste/src/test/java/com/marquezv/dev/apiTeste/ApiTesteApplicationTests.java
+++ apiTeste/apiTeste/src/test/java/com/marquezv/dev/apiTeste/ApiTesteApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApiTesteApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ApiTesteApplication.main(new String[] {});
 	}
 
 }
---
+++ apiTeste/apiTeste/src/test/java/com/marquezv/dev/apiTeste/service/UserServiceTest.java
@@ -0,0 +1,91 @@
+package com.marquezv.dev.apiTeste.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.marquezv.dev.apiTeste.domain.User;
+import com.marquezv.dev.apiTeste.domain.dto.UserDTO;
+import com.marquezv.dev.apiTeste.repository.UserRepository;
+
+@SpringBootTest
+public class UserServiceTest {
+	
+	private static final String PASSWORD = "123";
+
+	private static final String EMAIL = "vini@gmail.com";
+
+	private static final String USERNAME = "Vini";
+
+	private static final long ID = (long) 1;
+
+	@InjectMocks // Classe a ser testada
+	private UserService userService;
+	
+	@Mock // Classe auxiliar para o teste
+	private UserRepository repository;
+	
+	@Mock // Classe auxiliar para o teste
+	private ModelMapper mapper;
+	
+	private User user;
+	private UserDTO userDTO;
+	private Optional<User> optionalUser;
+	
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		startUser();
+	}
+	
+	@Test
+	void whenFindByIdThenReturnAnUserInstance() {
+		Mockito.when(
+				repository.findById(Mockito.anyLong()))
+		.thenReturn(optionalUser);
+		
+		User res = userService.findById(ID);
+		
+		assertNotNull(res);
+		assertEquals(User.class, res.getClass());
+		assertEquals(ID, res.getId());
+		assertEquals(USERNAME, res.getUsername());
+		assertEquals(EMAIL, res.getEmail());
+	}
+	
+	@Test
+	void findAll() {
+		
+	}
+
+	@Test
+	void create() {
+
+	}
+
+	@Test
+	void update() {
+		
+	}
+	
+	@Test
+	void delete() {
+		
+	}
+	
+	private void startUser() {
+		user = new User(ID, USERNAME, EMAIL, PASSWORD);
+		userDTO = new UserDTO(ID, USERNAME, EMAIL, PASSWORD);
+		optionalUser = Optional.of(new User(ID, USERNAME, EMAIL, PASSWORD));
+	}
+}
